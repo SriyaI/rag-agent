1eba1eb07b455fd11884720f7c2dc8c4cb1f02b7
sriya ivaturi <sriya@aihyr.com>
2024-11-10 22:33:22 +0530

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAt_VzXtOtwdXT5AG17Tvt2jcEweSMaTlo",
-  authDomain: "assignment-4c669.firebaseapp.com",
-  projectId: "assignment-4c669",
-  storageBucket: "assignment-4c669.appspot.com",
-  messagingSenderId: "88892215639",
-  appId: "1:88892215639:web:1d955e73c4696a72376e12",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
